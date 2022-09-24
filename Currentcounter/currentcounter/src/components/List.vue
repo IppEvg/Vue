572516c22b72fee8ee3a6ebdd6b14ce5e0060b9f
Evgeniy Ippolitov <ippevg@yandex.ru>
2022-09-24 23:50:33 +0300

    Homework3

---
+++ Currentcounter/currentcounter/src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class='cost-list'>
+        <div v-for="cost in list" v-bind:key="cost.id" class="cost-list__item">
+            <span>{{cost.id}}</span>
+            <span>{{cost.data}}</span>
+            <span>{{cost.category}}</span>
+            <span>{{cost.value}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    props: {
+        list: Array
+    }
+}
+</script>
+
+<style>
+.cost-list {
+    width: 500px;
+    margin: 0px auto;
+    display: flex;
+    flex-direction: column;
+}
+
+.cost-list__item {
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid rgb(170, 165, 165);
+    align-items: center;
+}
+</style>
